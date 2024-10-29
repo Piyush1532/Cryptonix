e5f312c6627974d39e9dd1597d34a8bc10682ecc
Piyush1532 <piyushadav503@gmail.com>
2024-10-29 15:18:12 +0530

    navbar functionality ready needed to improve some design basic design added to css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,33 @@ import { Link } from 'react-router-dom'
 import cryptonixLogo from "../../assets/cryptonixLogo.png"
 import arrow_icon from "../../assets/arrow_icon.png"
 import "./navbar.css"
+import { useDispatch, useSelector } from 'react-redux'
+import { setCurrency } from '../../Redux_Saga/coinSlice'
 const Navbar = () => {
+
+  const dispatch=useDispatch()
+  const currency=useSelector((state)=> state.coin.currency)
+
+
+
+  const currencyHandler = (event) => {
+
+    switch (event.target.value) {
+      case 'usd':
+        dispatch(setCurrency({ name: 'usd', symbol: '$' }));
+        break;
+      case 'eur':
+        dispatch(setCurrency({ name: 'eur', symbol: '€' }));
+        break;
+      case 'inr':
+        dispatch(setCurrency({ name: 'inr', symbol: '₹' }));
+        break;
+      default:
+        dispatch(setCurrency({ name: 'usd', symbol: '$' }));
+    }
+  };
+
+
   return (
     <>
           <div className='navbar'>
@@ -17,11 +43,11 @@ const Navbar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        {/* <select onChange={currencyHandler}>
+        <select onChange={currencyHandler} value={currency.name}>
             <option value="usd">USD</option>
             <option value="inr">INR</option>
             <option value="eur">EUR</option>
-        </select> */}
+        </select>
         <button>Sign Up <img src={arrow_icon} /></button>
       </div>
     </div>
